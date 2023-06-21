0a2c899f9c9b06af2108a451e7c92e4c2dc45cfc
VarkeyMampilly <137253488+VarkeyMampilly@users.noreply.github.com>
2023-06-21 00:22:29 -0400

    Update streamlit_app.py
    changed the streamlit.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Dinner is always late')
-streamlit.text ('Adding text ')
+streamlit.text ('what is cookine? ')
