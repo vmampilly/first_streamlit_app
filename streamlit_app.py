4e1c89b3dcba292080ce66c9097b395feffc696d
vmampilly <postboxno13@gmail.com>
2023-08-09 19:39:11 -0400

    Update streamlit_app.py
    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruites_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 # Use pandas to normalize the json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
