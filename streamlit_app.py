e074262f40740788ecfeac7f6c7f55f166b68f1a
vmampilly <postboxno13@gmail.com>
2023-08-10 00:42:38 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def get_fruityvice_data(this_fruit_choice):
 streamlit.header('FruityVice Fruite Advice!')
 try:
     fruit_choice=streamlit.txt_input('What fruite would you like information about?')
-    if not fruit_choice
+    if not fruit_choice:
         streamlit.error("Please select a fruit to get information")
 else:
         back_from_function=get_fruityvice_data(fruit_choice)
