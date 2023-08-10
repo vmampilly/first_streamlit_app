3c4cf5d73dab8553f2034c8880473606a2edc834
vmampilly <postboxno13@gmail.com>
2023-08-09 23:59:38 -0400

    Update streamlit_app.py
    fruit list from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,8 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The Fruit Load list Contains :")
 streamlit.text(my_data_row)
 
