efdf1c1fa86067a43d8c9cade071d4b8bf282444
vmampilly <postboxno13@gmail.com>
2023-08-10 00:41:58 -0400

    Update streamlit_app.py
    lot of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,33 @@ fruites_to_show=my_fruit_list.loc[fruites_selected]
 streamlit.dataframe(fruites_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-        streamlit.error ("please select a fruit to get information.")
-    else :
-        streamlit.write('The user entered ', fruit_choice)
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-        # streamlit.text(fruityvice_response.json())
-        
-        # Use pandas to normalize the json
+#try:
+#    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#    if not fruit_choice:
+#        streamlit.error ("please select a fruit to get information.")
+#    else :
+#        streamlit.write('The user entered ', fruit_choice)
+#        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#        # streamlit.text(fruityvice_response.json())
+#        
+#        # Use pandas to normalize the json
+#       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#       # display the data frame
+#       streamlit.dataframe(fruityvice_normalized)
+# create the repetable code block ( called a function)
+def get_fruityvice_data(this_fruit_choice):
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        # display the data frame
-        streamlit.dataframe(fruityvice_normalized)
+        return fruityvice_normalized
+#New section to display fruityvice api response
+streamlit.header('FruityVice Fruite Advice!')
+try:
+    fruit_choice=streamlit.txt_input('What fruite would you like information about?')
+    if not fruit_choice
+        streamlit.error("Please select a fruit to get information")
+else:
+        back_from_function=get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
 
